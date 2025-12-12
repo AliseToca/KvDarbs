765a5e5ad68e87dfdfe9ec6e7d90ea5c47082dd3
Alise Evelīna <alise.toca@cube.lv>
2025-12-12 03:17:04 +0200

    Add login/logout buttons in header

--- resources/js/Layouts/Partials/Header.vue
+++ resources/js/Layouts/Partials/Header.vue
@@ -1,17 +1,25 @@
 <script setup>
-import { usePage } from '@inertiajs/vue3'
-import TempLogoutButton from "../../Components/TempLogoutButton.vue";
+import {router, usePage} from '@inertiajs/vue3'
+import Button from "../../Components/Button.vue";
 
 const { headerMenu, languagePage, auth} = usePage().props;
 console.log('Inertia props:', usePage().props)
+
+const login = () => {
+    router.get('/login')
+}
+
+const logout = () => {
+    router.post('/logout')
+}
 
 </script>
 
 <template>
     <header class="site-header">
-        <a :href="`/${languagePage.slug}/`">{{languagePage?.content?.site_name}}</a>
+        <a :href="`/${languagePage.slug}/`">{{languagePage.content.site_name}}</a>
 
-        <nav v-if="headerMenu.length && auth?.user">
+        <nav v-if="headerMenu.length && auth.user">
             <ul>
                 <li v-for="item in headerMenu" :key="item.id">
                     <a :href="`/${languagePage.slug}/${item.page.slug}`">
@@ -20,6 +28,7 @@ console.log('Inertia props:', usePage().props)
                 </li>
             </ul>
         </nav>
-        <TempLogoutButton class="button primary"></TempLogoutButton>
+        <button v-if="auth.user" @click="logout" class="button primary">Logout</button>
+        <button v-else @click="login" class="button primary">Login</button>
     </header>
 </template>
