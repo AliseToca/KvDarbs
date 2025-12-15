4423fd823b2ac1ed23f9ce973c8dcf5e33a31031
Alise Evelīna <alise.toca@cube.lv>
2025-12-15 12:31:24 +0200

    Add simple footer

--- resources/js/Layouts/Partials/Footer.vue
+++ resources/js/Layouts/Partials/Footer.vue
@@ -1,7 +1,20 @@
 <script setup>
+import {usePage} from "@inertiajs/vue3";
 
+const { footerMenu, languagePage, auth} = usePage().props;
 </script>
 
 <template>
-    <p>This is a footer</p>
+    <footer class="site-footer">
+        <p>This is a footer</p>
+        <nav v-if="footerMenu.length && auth.user">
+            <ul>
+                <li v-for="item in footerMenu" :key="item.id">
+                    <a :href="`/${languagePage.slug}/${item.page.slug}`">
+                        {{ item.page.name }}
+                    </a>
+                </li>
+            </ul>
+        </nav>
+    </footer>
 </template>
