46dea8be7789ee0e7fcffa4cd46b1578a25aee5e
Alise Evelīna <alise.toca@cube.lv>
2026-01-21 16:52:32 +0200

    Add ziggy js to project

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,7 @@
 import { createApp, h } from 'vue';
 import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+import { ZiggyVue } from 'ziggy-js';
 import '../assets/scss/style.scss';
 import 'animate.css';
 import './bootstrap.ts';
@@ -17,6 +18,7 @@ createInertiaApp({
     setup({ el, App, props, plugin }) {
         createApp({ render: () => h(App, props) })
             .use(plugin)
+            .use(ZiggyVue)
             .mount(el);
     },
 });
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,6 +5,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     @vite(['resources/js/app.js'])
     @inertiaHead
+    @routes
 </head>
 <body class="antialiased">
     @inertia
