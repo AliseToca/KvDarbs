2b5948990ee5aad32807d8eb57cfd4317caa4025
Alise Evelīna <alise.toca@cube.lv>
2026-01-23 01:46:48 +0200

    Add maxLength check to InputField component

--- resources/js/Components/Inputs/InputField.vue
+++ resources/js/Components/Inputs/InputField.vue
@@ -9,14 +9,19 @@ const props = defineProps({
     placeholderValue: String,
     modelValue: String,
     error: [String, Array],
+    maxLength: {Number},
+    lengthCheck: Boolean,
 });
 
 const emit = defineEmits(['update:modelValue']);
 
-// Normalize error into an array (always)
 const errorList = computed(() => {
     if (!props.error) return [];
     return Array.isArray(props.error) ? props.error : [props.error];
+});
+
+const charCount = computed(() => {
+    return props.modelValue?.length || 0;
 });
 </script>
 
@@ -32,7 +37,12 @@ const errorList = computed(() => {
             :value="modelValue"
             @input="emit('update:modelValue', $event.target.value)"
             :class="{ 'input-error': errorList.length }"
+            :maxlength="maxLength"
         />
+
+        <div v-if="maxLength !== undefined" class="char-count">
+            {{ charCount }} / {{ maxLength }}
+        </div>
 
         <ul class="error-container">
             <li v-for="(err, i) in errorList" :key="i" class="error-message">
